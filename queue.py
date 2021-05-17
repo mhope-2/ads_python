578d32775cec72b26f6c91d49b5f1507f553645c
mhope <mhope@npontu.com>
2021-05-17 23:02:55 +0000

    feat(queue): added queue

--- queue.py
+++ queue.py
@@ -1,7 +1,7 @@
 from collections import deque
 
 my_queue = deque()
-my_queue.append(3) # add item to the end
+my_queue.append(5) # add item to the end
 my_queue.append(10) 
 print(my_queue)
 print(my_queue.popleft()) # pop item from the front
