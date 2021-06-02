9b52f59394a67bba25029b5346ac1e85a10ea4e6
mhope <mhope@npontu.com>
2021-06-02 09:45:14 +0000

    feat(linked_list): added linked list

--- linked_list.py
+++ linked_list.py
@@ -7,12 +7,14 @@ class LinkedList:
         self.size = 0
 
 
+    # add data
     def add(self, data):
         new_node = Node(data, next_node=self.root)
         self.root = new_node
         self.size +=1
 
-    
+
+    # find data
     def find(self, data):
         this_node = self.root
         while this_node is not None:
@@ -21,6 +23,27 @@ class LinkedList:
             else:
                 this_node = this_node.next_node
         return None
+
+
+    # remove data
+    def remove(self, data):
+        this_node = self.root
+        prev_node = None
+
+        while this_node is not None:
+            if this_node.data == data:
+                if prev_node is not None: # data is in non-root node
+                    prev_node.next_node = this_node.next_node
+                else: # data is in root node
+                    self.root = this_node.next_node
+                self.size -= 1
+                return True
+            else:
+                prev_node = this_node
+                this_node = this_node.next_node
+
+                
+
 
 
 
