9e47f1aef6e283fa590be635ddd224f751019d16
Michael Hope Setriakor <hopemichael720@gmail.com>
2021-12-21 15:55:58 +0000

    added balaced parentheses.py

---
+++ stack_ds/balanced_parentheses.py
@@ -0,0 +1,25 @@
+from stack import Stack
+
+def par_check(symbol_string):
+    stack = Stack()
+    balanced = True
+    index = 0
+
+    while index < len(symbol_string) and balanced:
+        symbol = symbol_string[index]
+        if symbol in "({[":
+            stack.push(symbol)
+        else:
+            if stack.is_empty():
+                balanced = False
+            else:
+                stack.pop()
+        index += 1
+
+    if balanced and stack.is_empty():
+        return True
+    else:
+        return False
+
+print(par_check("(()())"))
+
