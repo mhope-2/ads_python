52b3fd4c659f7be91a2886892d3006ae544f5f80
Michael Hope <hopemichael720@gmail.com>
2022-01-01 19:33:13 +0000

    added linked list

--- node.py
+++
@@ -1,8 +0,0 @@
-class Node:
-    def __init__(self, data, next_node=None, prev_node=None):
-        self.data = data
-        self.next_node = next_node
-        self.prev_node = prev_node
-
-    def __str__(self):
-        return ( '( {} )'.format(self.data))
--- readme.md
+++ readme.md
@@ -1,1 +1,10 @@
 ### Practicing Algorithms and Data Structures in Python
+
+Reference Material: 
+
+''' Problem Solving with Algorithms and
+    Data Structures
+    Release 3.0
+    Brad Miller, David Ranum 
+
+'''
