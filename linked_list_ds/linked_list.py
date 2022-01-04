42530b83a71353e1e79d9423799685ca965c0c13
Michael Hope <hopemichael720@gmail.com>
2022-01-04 00:22:40 +0000

    added linked list

--- linked_list_ds/linked_list.py
+++ linked_list_ds/linked_list.py
@@ -3,6 +3,8 @@ from .node import Node
 class LinkedList:
     def __init__(self) -> None:
         self.head = None
+        self.size = 0
+
     
     def is_empty(self):
         return self.head == None
@@ -11,3 +13,7 @@ class LinkedList:
       temp = Node(item)
       temp.set_next(self.head)
       self.head = temp
+      self.size += 1
+    
+    def remove():
+        pass
