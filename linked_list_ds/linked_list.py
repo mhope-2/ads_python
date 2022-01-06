09ba8990da9bdcb48e361fcd0bb3b68af083192e
Michael Hope <hopemichael720@gmail.com>
2022-01-06 23:52:10 +0000

    updated linked list

--- linked_list_ds/linked_list.py
+++ linked_list_ds/linked_list.py
@@ -3,7 +3,7 @@ from .node import Node
 class LinkedList:
     def __init__(self) -> None:
         self.head = None
-        self.size = 0
+        # self.size = 0
 
     
     def is_empty(self):
@@ -13,7 +13,16 @@ class LinkedList:
       temp = Node(item)
       temp.set_next(self.head)
       self.head = temp
-      self.size += 1
+    #   self.size += 1
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count += 1
+            current = current.get_next() 
+        
+
     
     def remove():
         pass
